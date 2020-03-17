b26adab6d0c2b8293454c87f3c80c2fce571456a
sreemikil <44523788+sreemikil@users.noreply.github.com>
2020-03-17 23:13:16 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+class choice
+{
+  string learner,mentor;
+    void addStacks()
+    {
+      stdout.writeln('Enter your stack of intrest\expertise');
+      String intrest = stdin.readLineSync();
+      print('your intrest is $intrest');
+    }
+    void setMentorOrLearner()
+    {
+      stdout.writeln('Enter choice as L for learner or M for mentor');
+      String choice = stdin.readLineSync();
+      print('i am a $choice');
+}
+void setAvailableTime()
+{
+stdout.writeln('enter the available time for class : (h:m) ');
+    Var hour = stdin.readLineSync();
+    Var min= stdin.readLineSync();
+stdout.writeln('your response is saved ');
+}
+getMentor()
+{
+stdout.writeln('your response void setAvailableTime() saved.we will inform if any mentors is available.');
+}
+Void main()
+{
+ void addStacks();
+void setMentorOrLearner();
+If(choice=='l');
+getMentor();
+else
+void setAvailableTime();
+}
